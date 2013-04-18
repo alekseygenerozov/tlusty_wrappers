7e5799cd8a570f87694f19e4a30318a5ae85edd1
Aleksey Generozov <aleksey.generozov@gmail.com>
2013-04-17 22:13:57 -0400

    Adjusting parameters and running tlusty (w/o input model and w/ LTE hard-coded

--- tlusty_tar/python/adjust.py
+++ tlusty_tar/python/adjust.py
@@ -1,4 +1,4 @@
-import tlusty_runs
+import tlusty_runs as tr
 import argparse
 import re
 import numpy as np
@@ -7,24 +7,46 @@ import astropy.table as at
 from astropy.io import ascii
 from types import *
 
-
-def adjust(p, start, delta, file='tmp.flag'):
+##Adjusts parameter p in 
+def adjust_file(p, end, file='tmp.flag'):
     #Open parameter file, read then close
     f=open(file, 'r')
     old=f.read()
     f.close()
-    #Constructing pattern to find in file and its replacement
-    #s0=p+"=[0-9.\-]*"  
-    #Substitute new value of parameter into file
-    s1=p+"="+str(start)
-    pat1=re.compile(s1)
-    s2=p+"="+str(start+delta)
-    pat2=re.compile(s2)
-    print s1,s2
-    new=pat1.sub(s2, old)
 
+    #Substitute new value of parameter into file
+    s1=p+"=[0-9.\-]*"
+    pat1=re.compile(s1)
+    s2=p+"="+str(end)
+    pat2=re.compile(s2)
+    print s2
+    new=pat1.sub(s2, old)
+    #Overwrite old file so that the parameter is adjusted like we wanted
     f=open(file, 'w')
     f.write(new)
+
+
+##Adjusts parameter p in tmp.flag until we have reached target
+def adjust(p, start, target, delta, inp_model):
+    there=np.allclose([start], [target])
+    print there
+    #if we are close to our target adjust delta accordingly
+    if there:
+        return
+    else:
+        if abs(target-start)<abs(delta):
+            delta=target-start
+
+        #End point of the current step
+        end=start+delta
+        adjust_file(p, end)
+        
+        tr.tlusty_runs()
+
+        start=end
+        adjust(p, start, target, delta, inp_model)
+
+
 
 
 
@@ -34,8 +56,13 @@ def adjust(p, start, delta, file='tmp.flag'):
 def main():
     parser=argparse.ArgumentParser(description='Wrapper to gradually adjust parameters')
     parser.add_argument('param')
-    parser.add_argument('delta')
     parser.add_argument('start')
+    parser.add_argument('target')
+    parser.add_argument('delta')
+    parser.add_argument('-i','--inp_model',
+        help='Input model atm',
+        action='store_const',
+        const=42)
     parser.add_argument('-f', '--floating', 
         help='Flag to turn on floating point adjustment to parameters',
         action='store_true')
@@ -43,19 +70,19 @@ def main():
     args=parser.parse_args()
     p=args.param
     floating=args.floating
+    inp_model=args.inp_model
+
+    delta=int(args.delta)
+    start=int(args.start)
+    target=int(args.target)    
     #if the user has specified a floating flag, set start and delta parameters to be floats
     if floating:
         delta=float(args.delta)
         start=float(args.start)
-    #otherwise they should be integers    
-    else:
-        delta=int(args.delta)
-        start=int(args.start)
-
+        target=float(args.target)
 
     print p, delta
-
-    adjust(p, start,delta)
+    adjust(p, start, target, delta, inp_model)
 
 
 
