54dda8a8fd706e8003dcce0c26f26052621771ce
Aleksey Generozov <aleksey.generozov@gmail.com>
2013-06-13 13:02:46 -0400

    tlusty_runs wrapper now copies checks if destination already exists. Parameter order fixed to be consistent with directory names. Finally, creates directory names with non-integers (for appropriate parameters.

--- tlusty_tar/python/tlusty_runs.py
+++ tlusty_tar/python/tlusty_runs.py
@@ -3,9 +3,26 @@
 import argparse
 import numpy as np
 import subprocess
+import re
 import astropy.table as at
 from astropy.io import ascii
 from types import*
+
+# Function to parse file name in order to get list of parameters
+def parse_file(file):
+    #Extracting parameters from file names
+    t=re.search("t[-+]?\d+",file)
+    t=re.search("[-+]?\d+",t.group(0))
+    
+    m=re.search("m[-+]?\d+",file)
+    m=re.search("[-+]?\d+",m.group(0))
+    
+    q=re.search("q[-+]?\d+",file)
+    q=re.search("[-+]?\d+",q.group(0))
+    
+    params=np.array([float(t.group(0)), float(m.group(0)), float(q.group(0))])
+    params=params/10.    
+    return params
 
 ##Run a command from the bash shell
 def bash_command(cmd):
@@ -28,7 +45,7 @@ def setup(log_qgrav, log_teff, log_dmtot, nlte, model, copy=True, tailname='tail
         f1=open('fort.1', 'w')
 
         #Tail of tlusty input file assumed to already be present in the current directory
-        print(tailname)
+        #print(tailname)
         tailf=open(tailname, 'r')
 
         f1.write('1\n')
@@ -36,9 +53,11 @@ def setup(log_qgrav, log_teff, log_dmtot, nlte, model, copy=True, tailname='tail
         qgrav=10.**log_qgrav
         teff=10.**log_teff
         dmtot=10.**log_dmtot
+
+        print log_teff,log_qgrav,log_dmtot
    
         out='{0} {1:8.7e} {2:8.7e} {3:8.7e}'.format(0, teff, qgrav, dmtot)
-        print out
+        #print out
         # fort.5 input file for tlusty
         f5.write(out + '\n')
         #Second line of input
@@ -48,20 +67,20 @@ def setup(log_qgrav, log_teff, log_dmtot, nlte, model, copy=True, tailname='tail
         f5.write(tail) 
 
         #Format strings with info on teff, dmtot, qgrav for creating folder
-        t=str(int(np.around(log_teff*10)))
-        m=str(int(np.around(log_dmtot*10)))
-        q=str(int(np.around(log_qgrav*10)))
+        t='{0:.3g}'.format(log_teff*10)
+        m='{0:.3g}'.format(log_dmtot*10)
+        q='{0:.3g}'.format(log_qgrav*10)
+
+        print t
         #Create a folder in which to store the tlusty output in 
         loc='t' + t + 'm' + m + 'q' + q
         bash_command('mkdir -p ' + loc)
 
         #If the location of the input model is not blank then copy model atmosphere to the current directory
         if model:
-           
+            #print "test"           
             process=bash_command('check ' + model + '/fort.7')
-
             if len(process.stdout.readlines())==0:
-                print 'test'
                 loc=''
                 return loc
             bash_command('cp ' + model + '/fort.7 ' + './fort.8')
@@ -69,7 +88,6 @@ def setup(log_qgrav, log_teff, log_dmtot, nlte, model, copy=True, tailname='tail
                 bash_command('cp ' + model + '/tmp.flag ' + './')
         #Return name of directory setup made   
         return loc
-
 
 
 ##Run TLUSTY from the bash shell
@@ -82,7 +100,6 @@ def reltot(file='fort.9'):
     dat=np.genfromtxt(file, skip_header=3)
 
     niter= dat[-1, 0]
-    print niter
     dat_cut=dat[(dat[:,0]==niter)]
 
 
@@ -90,7 +107,7 @@ def reltot(file='fort.9'):
          change=dat_cut[:, 2:7]
          change=np.array(map(np.abs, change))
          maxchange=change.max()
-         print maxchange
+         #print maxchange
     except ValueError:
          maxchange=1000
 
@@ -117,9 +134,21 @@ def clean(outdir,maxchange,nlte):
         dest=dest + outdir + '/lteconv'
     else:
         dest=dest + outdir + '/nconv'
-    print(dest)
-    #delete any files in the destination folder (i.e. from a previous run of tlusty)
-    bash_command('rm -f ' + dest + '/*')
+    #print(dest)
+    
+    #check for existence of destination file
+    i=2
+    dest_orig=dest
+    process=bash_command('check ' + dest)
+    #print len(process.stdout.readlines())
+    while len(process.stdout.readlines())!=0:
+        dest=dest_orig+"_"+str(i)
+        process=bash_command('check ' + dest)
+        i+=1
+        print i
+    print dest
+
+    #bash_command('rm -f ' + dest + '/*')
     bash_command('mkdir -p ' + dest)
     #moving all tlusty i/o files to the destination
     bash_command('mv ' + 'fort* ' + dest)
@@ -131,9 +160,10 @@ def clean(outdir,maxchange,nlte):
 
 ##Run tlusty based on command line input parameters
 def tlusty_runs_input(params, model, nlte=False, copy=True, combo=False, tailname='tail'):
-    log_qgrav=params[0]
-    log_teff=params[1]
-    log_dmtot=params[2]
+    log_teff=params[0]
+    log_dmtot=params[1]
+    log_qgrav=params[2]
+
 
     print log_teff,log_qgrav,log_dmtot
 
@@ -152,7 +182,6 @@ def tlusty_runs_input(params, model, nlte=False, copy=True, combo=False, tailnam
     #If we would like to calculate a combination on lte and nlte atmospheres
     if combo:
         nlte=True
-        print outdir
         outdir=setup(log_qgrav, log_teff, log_dmtot, nlte, outdir, True, tailname)
         if not outdir: 
             return
@@ -165,6 +194,9 @@ def tlusty_runs_input(params, model, nlte=False, copy=True, combo=False, tailnam
 
 ##Run tlusty based on parameters found at the location of model
 def tlusty_runs_model(model, nlte=False, copy=True, tailname='tail'):
+    process=bash_command('check ' + model + '/fort.5')
+    if len(process.stdout.readlines())==0:
+        return
     params=ascii.read(model + '/fort.5', data_start=0, data_end=1)
     #print params
     log_teff=np.log10(params[0][1])
@@ -196,10 +228,10 @@ def tlusty_runs_file(myfile='params.in', nlte=False, copy=True, combo=False, tai
     for i in range(0, len(params)): 
         f=open('fort.5', 'w')
         tailf=open('tail', 'r')
-        print(params[i])
-        log_qgrav=params[i][0]
-        log_teff=params[i][1]
-        log_dmtot=params[i][2]
+
+        log_teff=params[i][0]
+        log_dmtot=params[i][1]
+        log_qgrav=params[i][2]
 
         if ncols>3:
             model=params[i][3]
@@ -243,7 +275,7 @@ def main():
         help='Switch on nlte',
         action='store_true')
     parser.add_argument('-c','--combo',
-        help='Combination of lte and nlte models when reading from input file',
+        help='Combination of lte and nlte models when reading from input file or using command line parameters',
         action='store_true')
     parser.add_argument('-f', '--file', 
         help='File containing input parameters; default is params.in', 
@@ -258,8 +290,8 @@ def main():
         help='Stores name of file containing atomic data.',
         default='tail')
     parser.add_argument('-p', '--params',
-        metavar=('qgrav', 'teff', 'dmtot'),
-        help='Stores required parameters for atmosphere. Need 3 positional arguments in the order qgrav, teff, dmtot',
+        metavar=('teff', 'dmtot', 'qgrav'),
+        help='Stores required parameters for atmosphere. Need 3 positional arguments in the order teff, dmtot, qgrav',
         nargs=3,
         type=float)
     args=parser.parse_args()
@@ -272,15 +304,9 @@ def main():
     combo=args.combo
     tailname=args.tail
 
-    print(tailname)
-
-
-
-
     if params:
         tlusty_runs_input(params, model, nlte, copy, combo, tailname)
     elif  model:
-        print 'test'
         tlusty_runs_model(model, nlte, copy, tailname)
     else:
         tlusty_runs_file(myfile, nlte, copy, combo, tailname)
