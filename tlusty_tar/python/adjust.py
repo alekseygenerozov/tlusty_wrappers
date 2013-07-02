cf7fa26fcf4bf24a1823328e3d14756f126f53f6
Aleksey Generozov <aleksey.generozov@gmail.com>
2013-07-02 15:34:13 -0400

    Modified the parameter adjustment routine so that model for last value of parameter is explicitly specified as model for the next iteration, also no need for separate flag to handle floating point parameters

--- tlusty_tar/python/adjust.py
+++ tlusty_tar/python/adjust.py
@@ -15,41 +15,49 @@ def adjust_file(p, end, file='tmp.flag'):
     f=open(file, 'r')
     old=f.read()
     f.close()
-
     #Substitute new value of parameter into file
-    s1=p+"=[0-9.\-]*"
+    s1=p+'=[0-9.\-]*'
+    s2=p+'='+'{0:.3g}'.format(end)
     pat1=re.compile(s1)
-    s2=p+"="+str(end)
-    pat2=re.compile(s2)
-    print s2
-    new=pat1.sub(s2, old)
-    #Overwrite old file so that the parameter is adjusted like we wanted
-    f=open(file, 'w')
-    f.write(new)
+
+    #print old
+    #print pat1.search(old)
+    #If the parameter is present in the old file then substitute the new value 
+    if pat1.search(old):
+        pat2=re.compile(s2)
+        new=pat1.sub(s2, old)
+        f=open(file, 'w')
+        f.write(new) 
+    #If the parameter is not present then simply append it to the end of the optional parameters file
+    f=open(file, 'a')
+    f.write(s2+'\n')
 
 
-##Adjusts parameter p in tmp.flag until we have reached target
+## Adjusts parameter p in tmp.flag until we have reached target
 def adjust(p, start, target, delta, model, nlte=False):
     there=np.allclose([start], [target])
-    #if we are close to our target adjust delta accordingly
+    print model
+    #if we are there then done
     if there:
         return
+    #if close to target then adjust the step appropriately
     else:
         if abs(target-start)<abs(delta):
             delta=target-start
 
         #End point of the current step
         end=start+delta
+        print end
         #Copy the original optional parameters file 
         if model:
             tr.bash_command('cp ' + model + '/tmp.flag ' + './')
         adjust_file(p, end)
 
-
-
         maxchange=1
         if model:
-            maxchange=tr.tlusty_runs_model(model, nlte=nlte, copy=False)
+            out=tr.tlusty_runs_model(model, nlte=nlte, copy=False)
+            maxchange=out[0]
+            model=out[1]
             print maxchange
         else:
             maxchange=tr.tlusty_runs_file(nlte=nlte, copy=False)
@@ -59,16 +67,11 @@ def adjust(p, start, target, delta, model, nlte=False):
         if np.isnan(maxchange):
             maxchange=1000
         if maxchange>0:
-            print 'Nominal convergence does not look good chief'
+            print 'Atmosphere does not appear to converge'
             return
 
         start=end
         adjust(p, start, target, delta, model, nlte)
-
-
-
-
-
 
 
 ##driver; parse user input
@@ -80,29 +83,31 @@ def main():
     parser.add_argument('delta')
     parser.add_argument('-m','--model',
         help='Model atmosphere location')
-    parser.add_argument('-fl', '--floating', 
-        help='Flag to turn on floating point adjustment to parameters',
-        action='store_true')
+    # parser.add_argument('-fl', '--floating', 
+    #     help='Flag to turn on floating point adjustment to parameters',
+    #     action='store_true')
     parser.add_argument('-n','--nlte',
         help='Switch on nlte',
         action='store_true')
     #parsing the input arguments
     args=parser.parse_args()
     p=args.param
-    floating=args.floating
+    #floating=args.floating
     model=args.model
     nlte=args.nlte
 
-
     #if the user has specified a floating flag, set start and delta parameters to be floats
-    if floating:
-        delta=float(args.delta)
-        start=float(args.start)
-        target=float(args.target)
-    else:
-        delta=int(args.delta)
-        start=int(args.start)
-        target=int(args.target)    
+    delta=float(args.delta)
+    start=float(args.start)
+    target=float(args.target)
+    # if floating:
+    #     delta=float(args.delta)
+    #     start=float(args.start)
+    #     target=float(args.target)
+    # else:
+    #     delta=int(args.delta)
+    #     start=int(args.start)
+    #     target=int(args.target)    
 
     adjust(p, start, target, delta, model, nlte)
 
