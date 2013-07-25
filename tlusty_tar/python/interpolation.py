0c4332fc688b94ae82e52c7305c9b0685748634d
Aleksey Generozov <aleksey.generozov@gmail.com>
2013-07-25 14:46:49 -0400

    Moved version of interpolation.py did not include additions corresponding to graybody

--- tlusty_tar/python/interpolation.py
+++ tlusty_tar/python/interpolation.py
@@ -1,11 +1,17 @@
+#!/usr/bin/env python
+
 from scipy.interpolate import griddata
 import numpy as np
 import re
 
 from matplotlib import rc
 import matplotlib.pyplot as plt
+from matplotlib.backends.backend_pdf import PdfPages
 
 import argparse
+import subprocess
+
+import graybody as gray
 
 
 
@@ -14,6 +20,12 @@ c=3*10**10
 h=6.67*10**-27
 kb=1.38*10**-16
 
+
+##Run a command from the bash shell
+def bash_command(cmd):
+     process=subprocess.Popen(['/bin/bash', '-c', cmd],  stdout=subprocess.PIPE)
+     process.wait()
+     return process
 
 # Function to extract frequency from wavelength in angstroms
 def get_freq(w):
@@ -50,6 +62,7 @@ def get_spec(file, nfreq=300, mu=-1, nmu=10):
     
     spec=spec[::2]
     wl=wlh[::2]
+
     
     #mu is and index for the polar angle to use. -1 corresponds to returning emergent flux from the disk
     if mu == -1:
@@ -60,6 +73,8 @@ def get_spec(file, nfreq=300, mu=-1, nmu=10):
         
     spec=spec.reshape(( nfreq, nmu))   
     spec=spec[:, mu]
+
+    print np.shape(wl), np.shape(spec), f
     
     spec=np.vstack((wl,spec)) 
     return spec
@@ -130,14 +145,11 @@ def construct_table(models):
     spec=map(get_spec, models)
     spec=np.array(spec)
     spec=map(regrid, spec)
-    spec=np.array(spec)
 
-    spec[:, 1]=np.log10(spec[:, 1])
-
-    #spec_inv=map(bright_spec, spec)
-    #spec_inv=np.array(spec_inv)
+    spec_inv=map(bright_spec, spec)
+    spec_inv=np.array(spec_inv)
     
-    return (params, spec)
+    return (params, spec_inv)
 
     
 # Get parameters corresponding to our disk
@@ -148,13 +160,12 @@ def get_params(file):
 # Use table construct spectra from list of parameters
 def params_to_spec(params, table):
     grid2=griddata(table[0], table[1], params)#, fill_value=1.e-36)
-    grid2[0,1]=10.**grid2[0,1]
-    #print grid2[0,1]
+    grid2=map(bright_inv,grid2)
     
     return np.array(grid2)
 
 # Computes composite spectrum given array of radii, spectra. Also computes corresponding blackbody spectrum using list of Teff that are passed to the function
-def sum_spec(r, specs, Teff):   
+def sum_spec(r, specs, Teff, Qg):   
     dr=r[:, 0]
     r=r[:, 1]
     nu=specs[0,0]
@@ -162,13 +173,15 @@ def sum_spec(r, specs, Teff):
         
     L=np.zeros(len(nu))
     bb=np.zeros(len(nu))
+    gb=np.zeros(len(nu))
     for i in range(len(r)):
         for j in range(len(nu)):
             L[j]+=2*np.pi*r[i]*dr[i]*f[i,j]
             bb[j]+=2*np.pi*r[i]*dr[i]*(np.pi*Tb_inv(nu[j], Teff[i], fcol=1))
+            gb[j]+=2*np.pi*r[i]*dr[i]*(gray.gb(nu[j], 10.**Teff[i], 10.**Qg[i]))
             #bb[j]*=2*np.pi*r[i]*dr[i]
         
-    return (np.vstack([nu, bb]), np.vstack([nu, L])) 
+    return (np.vstack([nu, gb]),np.vstack([nu, bb]), np.vstack([nu, L])) 
     #return np.vstack([nu, (nu*2*np.pi*r*dr*f).sum(axis=0)])
 
 
@@ -176,19 +189,22 @@ def sum_spec(r, specs, Teff):
 def test_spec(f, table=[], tablef='tmpd'):
     if table==[]:
         table=construct_table(tablef)
-    print table[1]
+    #print table[0]
     testspec=get_spec(f)
     testspec=regrid(testspec)
     
     params=parse_file(f)
     params_s='t'+str(10*params[0])+'m'+str(10*params[1])+'q'+str(10*params[2])
+    print params
     params=params[::2]
     
+
     testspec2=params_to_spec([params], table)
     testspec2=testspec2[0]
 
-    plt.figure()
+    fig=plt.figure()
     plt.loglog()
+    #plt.figsize(20, 8) 
     plt.xlabel('nu [hz]')
     plt.ylabel("nu F_nu [ergs/s/cm^2]")
     plt.axis([10.**14, 2*10.**18, 10.**6, 10.**16])
@@ -197,6 +213,7 @@ def test_spec(f, table=[], tablef='tmpd'):
     plt.plot(testspec[0], testspec[0]*testspec[1])
     plt.plot(testspec2[0], testspec2[0]*testspec2[1])
     plt.savefig(params_s+'.pdf')
+    return fig
     #plt.show()
 
 
@@ -209,53 +226,68 @@ def disk_spec(f, tablef='tmpd'):
 
     r=disk_params[:, 0:2]
     Teff=disk_params[:, 2]
+    Qg=disk_params[:, 4]
 
     #Finding the total flux
-    totf=sum_spec(r, specs, Teff)
-    totf1=totf[1]
-    totf2=totf[0]
+    totf=sum_spec(r, specs, Teff, Qg)
+    totfg=totf[0]
+    totfb=totf[1]
+    totft=totf[2]
+    #totfg2=np.genfromtxt('gray_test')
+    
 
+    fig=plt.figure()
     plt.loglog()
     #plt.figsize(20, 8)
     plt.xlabel("nu [hz]")
     plt.ylabel("nu L_nu [ergs/s]")
-    plt.axis([10.**14, 2*10.**18, 10.**38, 10.**44]) 
-    p
+    plt.axis([10.**14, 2*10.**18, 10.**38, 10.**45]) 
 
-    plt.plot(totf1[0], totf1[0]*totf1[1])
-    plt.plot(totf2[0], totf2[0]*totf2[1])
+    plt.plot(totfg[0], totfg[0]*totfg[1])
+    plt.plot(totfb[0], totfb[0]*totfb[1])
+    plt.plot(totft[0], totft[0]*totft[1])
+    #plt.plot(totfg2[:,0], totfg2[:,0]*totfg2[:,1])
     plt.show()
+    return fig
 
 def main():
     parser=argparse.ArgumentParser(
-        description='Computes composite disk spectrum based on TLUSTY disk spectra')
-    parser.add_argument('f',
+        description='Either takes list of disk parameters and computes composite disk spectrum from table or compares spectra interpolated'+
+          ' from table to those in a test directory')
+    parser.add_argument('-f', '--file',
         help='file name containing radial disk profile ',
-        type=str)
+        default='')
+    parser.add_argument('-d', '--dir',
+        help='directory containing the location of models to test',
+        default='')
 
     args=parser.parse_args()
-    f=args.f
+    f=args.file
+    d=args.dir
 
-    disk_spec(f)
+    pdf_pages = PdfPages('out.pdf')
+
+    if f:
+        fig=disk_spec(f)
+        pdf_pages.savefig(fig)
+    elif d:
+        process=bash_command('ls '+d)
+        for m in process.stdout.readlines():
+            m=m.rstrip()
+            fig=test_spec('./'+d+'/'+m)
+            pdf_pages.savefig(fig)
+    else:
+        parser.print_help()
+
+    pdf_pages.close()
+
+
 
 if __name__ == '__main__':
     main()
     
     
-# def Knu(es, f):
-#     return -0.3333333333333333 + (0.41997368329829105*(-1. + 2.*es - \
-#     1.*es**2))/(2. - 12.*es + 30.*es**2 - 40.*es**3 + 30.*es**4 - \
-#     12.*es**5 + 2.*es**6 - 27.*es**2*f + 108.*es**3*f - 162.*es**4*f + \
-#     108.*es**5*f - 27.*es**6*f + np.sqrt(-4.*(-1. + 2.*es - 1.*es**2)**6 + \
-#     (2. - 12.*es + 30.*es**2 - 40.*es**3 + 30.*es**4 - 12.*es**5 + \
-#     2.*es**6 - 27.*es**2*f + 108.*es**3*f - 162.*es**4*f + 108.*es**5*f - \
-#     27.*es**6*f)**2))**0.3333333333333333 + (0.26456684199469993*(2. - \
-#     12.*es + 30.*es**2 - 40.*es**3 + 30.*es**4 - 12.*es**5 + 2.*es**6 - \
-#     27.*es**2*f + 108.*es**3*f - 162.*es**4*f + 108.*es**5*f - \
-#     27.*es**6*f + np.sqrt(-4.*(-1. + 2.*es - 1.*es**2)**6 + (2. - 12.*es + \
-#     30.*es**2 - 40.*es**3 + 30.*es**4 - 12.*es**5 + 2.*es**6 - \
-#     27.*es**2*f + 108.*es**3*f - 162.*es**4*f + 108.*es**5*f - \
-#     27.*es**6*f)**2))**0.3333333333333333)/(-1. + 2.*es - 1.*es**2)
+
     
 
 # def Knu(es, f):
